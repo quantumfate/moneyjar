7d082da2789c75dead335a3c68b9b411248fcb82
Leon Connor Holm <leonconnorholm@gmail.com>
2022-12-19 01:45:53 +0100

    added appropriate name

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene import ObjectType, Schema
 
-from backend.api.queries.queries import Query
+from backend.api.queries.base import Query
 
 # Attach the root query to the schema
 schema = graphene.Schema(query=Query)
